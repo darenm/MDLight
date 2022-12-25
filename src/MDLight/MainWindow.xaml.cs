a811dc0c3b5a66e3fc6a60157bcd0ac3606da13c
Daren May <daren.may@gmail.com>
2022-12-25 16:07:26 -0700

    Adding backdrop and toolbar customization

--- src/MDLight/MainWindow.xaml.cs
+++ src/MDLight/MainWindow.xaml.cs
@@ -1,22 +1,23 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using MDLight.Utilities;
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Runtime.InteropServices;
+
+using MDLight.Utilities;
+
+using Microsoft.UI;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+using WinRT;
+using WinRT.Interop;
+
+using AppWindow = Microsoft.UI.Windowing.AppWindow;
+using AppWindowTitleBar = Microsoft.UI.Windowing.AppWindowTitleBar;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,13 +29,104 @@ namespace MDLight
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private AppWindow _appWindow;
+        WindowsSystemDispatcherQueueHelper _wsdqHelper; // See below for implementation.
+        ISystemBackdropControllerWithTargets _backdropController;
+        SystemBackdropConfiguration _configurationSource;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            _appWindow = GetAppWindowForCurrentWindow();
+            _appWindow.Title = "MDLight";
             WindowHelper.RegisterWindowMinMax(this);
             WindowHelper.MinWindowWidth = 400;
             WindowHelper.MinWindowHeight = 400;
             WindowHelper.SetWindowSize(this, 900, 600);
+
+            // Check to see if customization is supported.
+            // Currently only supported on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                TitleTextBlock.Text = "MDLight";
+                var titleBar = _appWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                AppTitleBar.Loaded += AppTitleBar_Loaded;
+                AppTitleBar.SizeChanged += AppTitleBar_SizeChanged;
+
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonForegroundColor = Colors.White;
+            }
+            else
+            {
+                // Title bar customization using these APIs is currently
+                // supported only on Windows 11. In other cases, hide
+                // the custom title bar element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+
+                // Show alternative UI for any functionality in
+                // the title bar, such as search.
+                SetTitleBarColors();
+            }
+
+            TrySetSystemMicaBackdrop();
+        }
+
+        private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                SetDragRegionForCustomTitleBar(_appWindow);
+            }
+        }
+
+        private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && _appWindow.TitleBar.ExtendsContentIntoTitleBar)
+            {
+                // Update drag region if the size of the title bar changes.
+                SetDragRegionForCustomTitleBar(_appWindow);
+            }
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+
+        private bool SetTitleBarColors()
+        {
+            // Check to see if customization is supported.
+            // Currently only supported on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                if (_appWindow is null)
+                {
+                    _appWindow = GetAppWindowForCurrentWindow();
+                }
+                var titleBar = _appWindow.TitleBar;
+
+                // Set active window colors
+                titleBar.ForegroundColor = Colors.White;
+                titleBar.BackgroundColor = Colors.Green;
+                titleBar.ButtonForegroundColor = Colors.White;
+                titleBar.ButtonBackgroundColor = Colors.SeaGreen;
+                titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+                titleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
+                titleBar.ButtonPressedForegroundColor = Colors.Gray;
+                titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+
+                // Set inactive window colors
+                titleBar.InactiveForegroundColor = Colors.Gainsboro;
+                titleBar.InactiveBackgroundColor = Colors.SeaGreen;
+                titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                titleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
+                return true;
+            }
+            return false;
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
@@ -55,5 +147,269 @@ namespace MDLight
         {
 
         }
+
+
+        [DllImport("Shcore.dll", SetLastError = true)]
+        internal static extern int GetDpiForMonitor(IntPtr hmonitor, Monitor_DPI_Type dpiType, out uint dpiX, out uint dpiY);
+
+        internal enum Monitor_DPI_Type : int
+        {
+            MDT_Effective_DPI = 0,
+            MDT_Angular_DPI = 1,
+            MDT_Raw_DPI = 2,
+            MDT_Default = MDT_Effective_DPI
+        }
+
+        private double GetScaleAdjustment()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            DisplayArea displayArea = DisplayArea.GetFromWindowId(wndId, DisplayAreaFallback.Primary);
+            IntPtr hMonitor = Win32Interop.GetMonitorFromDisplayId(displayArea.DisplayId);
+
+            // Get DPI.
+            int result = GetDpiForMonitor(hMonitor, Monitor_DPI_Type.MDT_Default, out uint dpiX, out uint _);
+            if (result != 0)
+            {
+                throw new Exception("Could not get DPI for monitor.");
+            }
+
+            uint scaleFactorPercent = (uint)(((long)dpiX * 100 + (96 >> 1)) / 96);
+            return scaleFactorPercent / 100.0;
+        }
+
+        private void SetDragRegionForCustomTitleBar(AppWindow appWindow)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && appWindow.TitleBar.ExtendsContentIntoTitleBar)
+            {
+                double scaleAdjustment = GetScaleAdjustment();
+
+                RightPaddingColumn.Width = new GridLength(appWindow.TitleBar.RightInset / scaleAdjustment);
+                LeftPaddingColumn.Width = new GridLength(appWindow.TitleBar.LeftInset / scaleAdjustment);
+
+                List<Windows.Graphics.RectInt32> dragRectsList = new();
+
+                Windows.Graphics.RectInt32 dragRectL;
+                dragRectL.X = (int)((LeftPaddingColumn.ActualWidth) * scaleAdjustment);
+                dragRectL.Y = 0;
+                dragRectL.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectL.Width = (int)((IconColumn.ActualWidth
+                                        + TitleColumn.ActualWidth
+                                        + LeftDragColumn.ActualWidth) * scaleAdjustment);
+                dragRectsList.Add(dragRectL);
+
+                Windows.Graphics.RectInt32 dragRectR;
+                dragRectR.X = (int)((LeftPaddingColumn.ActualWidth
+                                    + IconColumn.ActualWidth
+                                    + TitleTextBlock.ActualWidth
+                                    + LeftDragColumn.ActualWidth
+                                    + SearchColumn.ActualWidth) * scaleAdjustment);
+                dragRectR.Y = 0;
+                dragRectR.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectR.Width = (int)(RightDragColumn.ActualWidth * scaleAdjustment);
+                dragRectsList.Add(dragRectR);
+
+                Windows.Graphics.RectInt32[] dragRects = dragRectsList.ToArray();
+
+                appWindow.TitleBar.SetDragRectangles(dragRects);
+            }
+        }
+
+        private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
+        {
+            if (args.DidPresenterChange
+                && AppWindowTitleBar.IsCustomizationSupported())
+            {
+                switch (sender.Presenter.Kind)
+                {
+                    case AppWindowPresenterKind.CompactOverlay:
+                        // Compact overlay - hide custom title bar
+                        // and use the default system title bar instead.
+                        AppTitleBar.Visibility = Visibility.Collapsed;
+                        sender.TitleBar.ResetToDefault();
+                        break;
+
+                    case AppWindowPresenterKind.FullScreen:
+                        // Full screen - hide the custom title bar
+                        // and the default system title bar.
+                        AppTitleBar.Visibility = Visibility.Collapsed;
+                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
+                        break;
+
+                    case AppWindowPresenterKind.Overlapped:
+                        // Normal - hide the system title bar
+                        // and use the custom title bar instead.
+                        AppTitleBar.Visibility = Visibility.Visible;
+                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
+                        SetDragRegionForCustomTitleBar(sender);
+                        break;
+
+                    default:
+                        // Use the default system title bar.
+                        sender.TitleBar.ResetToDefault();
+                        break;
+                }
+            }
+        }
+
+        private void SwitchPresenter(object sender, RoutedEventArgs e)
+        {
+            if (_appWindow != null)
+            {
+                AppWindowPresenterKind newPresenterKind;
+                switch ((sender as Button).Name)
+                {
+                    case "CompactoverlaytBtn":
+                        newPresenterKind = AppWindowPresenterKind.CompactOverlay;
+                        break;
+
+                    case "FullscreenBtn":
+                        newPresenterKind = AppWindowPresenterKind.FullScreen;
+                        break;
+
+                    case "OverlappedBtn":
+                        newPresenterKind = AppWindowPresenterKind.Overlapped;
+                        break;
+
+                    default:
+                        newPresenterKind = AppWindowPresenterKind.Default;
+                        break;
+                }
+
+                // If the same presenter button was pressed as the
+                // mode we're in, toggle the window back to Default.
+                if (newPresenterKind == _appWindow.Presenter.Kind)
+                {
+                    _appWindow.SetPresenter(AppWindowPresenterKind.Default);
+                }
+                else
+                {
+                    // Else request a presenter of the selected kind
+                    // to be created and applied to the window.
+                    _appWindow.SetPresenter(newPresenterKind);
+                }
+            }
+        }
+
+        bool TrySetSystemMicaBackdrop()
+        {
+            if (MicaController.IsSupported())
+            {
+                _wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+                _wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+                // Create the policy object.
+                _configurationSource = new SystemBackdropConfiguration();
+                this.Activated += Window_Activated;
+                this.Closed += Window_Closed;
+                ((FrameworkElement)this.Content).ActualThemeChanged += Window_ThemeChanged;
+
+                // Initial configuration state.
+                _configurationSource.IsInputActive = true;
+                SetConfigurationSourceTheme();
+
+                SetMica();
+
+                // Enable the system backdrop.
+                // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+                _backdropController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                _backdropController.SetSystemBackdropConfiguration(_configurationSource);
+                return true; // succeeded
+            }
+
+            return false; // Mica is not supported on this system
+        }
+  
+
+        private void SetAcrylic()
+        {
+            _backdropController = new DesktopAcrylicController();
+        }
+
+        private void SetMicaWithAlt()
+        {
+            _backdropController = new MicaController()
+            {
+                Kind = MicaKind.BaseAlt
+            };
+        }
+
+        private void SetMica()
+        {
+            _backdropController = new MicaController();
+        }
+
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            _configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            // Make sure any Mica/Acrylic controller is disposed
+            // so it doesn't try to use this closed window.
+            if (_backdropController != null)
+            {
+                _backdropController.Dispose();
+                _backdropController = null;
+            }
+            this.Activated -= Window_Activated;
+            _configurationSource = null;
+        }
+
+        private void Window_ThemeChanged(FrameworkElement sender, object args)
+        {
+            if (_configurationSource != null)
+            {
+                SetConfigurationSourceTheme();
+            }
+        }
+
+        private void SetConfigurationSourceTheme()
+        {
+            switch (((FrameworkElement)this.Content).ActualTheme)
+            {
+                case ElementTheme.Dark: _configurationSource.Theme = SystemBackdropTheme.Dark; break;
+                case ElementTheme.Light: _configurationSource.Theme = SystemBackdropTheme.Light; break;
+                case ElementTheme.Default: _configurationSource.Theme = SystemBackdropTheme.Default; break;
+            }
+        }
+    }
+
+    class WindowsSystemDispatcherQueueHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct DispatcherQueueOptions
+        {
+            internal int dwSize;
+            internal int threadType;
+            internal int apartmentType;
+        }
+
+        [DllImport("CoreMessaging.dll")]
+        private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+        object m_dispatcherQueueController = null;
+        public void EnsureWindowsSystemDispatcherQueueController()
+        {
+            if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+            {
+                // one already exists, so we'll just use it.
+                return;
+            }
+
+            if (m_dispatcherQueueController == null)
+            {
+                DispatcherQueueOptions options;
+                options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+                options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+                options.apartmentType = 2; // DQTAT_COM_STA
+
+                CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+            }
+        }
     }
 }
+
+
